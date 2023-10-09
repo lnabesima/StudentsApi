c7f914522d6749f2569660ad38f827d470594bbe
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-09 16:35:41 -0300

    feat: create Student model

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsApi.Models;
+
+public class Student
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(80)]
+    public string? Name { get; set; }
+    [Required]
+    [EmailAddress]
+    [StringLength(120)]
+    public string? Email { get; set; }
+    [Required]
+    public int Age { get; set; }
+}
