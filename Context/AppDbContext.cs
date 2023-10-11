fc36ae4449f8aef459dcd7730e1de60097056a48
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-10-11 06:09:48 -0300

    feat: create a new migration to populate the students table

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -10,4 +10,24 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>().HasData(
+        new Student
+        {
+            Id = 1,
+            Name = "Maria da Penha",
+            Email = "mariapenha@gmail.com",
+            Age = 23,
+        },
+        new Student
+        {
+            Id = 2,
+            Name = "Manuel Bueno",
+            Email = "manuelbueno@gmail.com",
+            Age = 24,
+        }
+            );
+    }
 }
---
+++ Migrations/20231011090743_PopulateStudents.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudentsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateStudents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, 23, "mariapenha@gmail.com", "Maria da Penha" },
+                    { 2, 24, "manuelbueno@gmail.com", "Manuel Bueno" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
